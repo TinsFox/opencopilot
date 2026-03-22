5e2940052b91fad6a492e81937307817991e3ed6
TinsFox <fox@tinsfox.com>
2026-03-22 12:31:28 +0800

    feat(web): add shadcn ui foundation

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,10 +4,14 @@ import { RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { TooltipProvider } from '@/components/ui/tooltip'
+
 import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <TooltipProvider>
+      <RouterProvider router={router} />
+    </TooltipProvider>
   </StrictMode>,
 )
